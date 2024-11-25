179ed836400d226f3c5c439b6fab4fa6339c6dc8
Davi <00001123644263sp@al.educacao.sp.gov.br>
2024-11-24 22:14:37 -0300

    Upload dos arquivos do projeto

---
+++ Flashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da Russia é Moscou'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz foi bom te conhecer em Inglês?',
+    'Foi bom te conhecer em ingles é it was good to meet you'
+)
